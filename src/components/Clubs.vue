f171fccbe0016dd7e003ff8b2241609a85edb07a
Pavel Shukhman <taleodor@gmail.com>
2020-05-20 21:52:49 -0400

    fix: add Russian Speaking clubs

--- src/components/Clubs.vue
+++ src/components/Clubs.vue
@@ -2,11 +2,18 @@
     <div>
         <h1>Classic Mafia Clubs</h1>
         <div class="clubs">
-            <p>This page contains information about English Speaking Classic Mafia clubs.</p>
-            <p>If you want your club added here, contact <a href="https://twitter.com/taleodor">@taleodor</a>.</p>
+            <p>This page contains information about Classic Mafia clubs.</p>
+            <p>If you would like to see your club added here, contact <a href="https://twitter.com/taleodor">@taleodor</a>.
+            Or if you are tech-savvy, submit a pull request <a href="https://github.com/taleodor/mafia-vue/blob/master/src/components/Clubs.vue">here</a>.</p>
+            <h4>English Speaking Clubs:</h4>
             <ul>
                 <li>Vancouver Mafia Club - <a href="https://www.facebook.com/groups/226449514517735/">https://www.facebook.com/groups/226449514517735/</a></li>
                 <li>Kiev English Mafia Club - <a href="https://englishmafiaclub.com/">https://englishmafiaclub.com/</a></li>
+            </ul>
+            <h4>Russian Speaking Clubs:</h4>
+            <ul>
+                <li>Freud Mafia - <a href="https://www.facebook.com/FreudMafia/">https://www.facebook.com/FreudMafia/</a></li>
+                <li>Mafia Club in Ottawa - <a href="https://www.facebook.com/groups/mafiagameottawa/">https://www.facebook.com/groups/mafiagameottawa/</a></li>
             </ul>
         </div>
     </div>
